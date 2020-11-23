1ee5692c7e00d0cb95443acbe5cfc2bf6236e774
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-11-23 18:07:22 +0700

    Update l3.js

--- js/l3.js
+++ js/l3.js
@@ -1,6 +1,5 @@
-document.getElementById('footby1').innerHTML = 'Powered by'
 // Back to top
 $(function(){$(window).scroll(function(){$(this).scrollTop()>400?$(".back-to-top").addClass('arlniainf'):$(".back-to-top").removeClass('arlniainf')}),$(".back-to-top").click(function(){return $("html,body").animate({scrollTop:0},400),!1})});
 // Footer
-document.getElementById('footby2').innerHTML = 'Blogger'
+document.getElementById('footby').innerHTML = 'Blogger'
 
