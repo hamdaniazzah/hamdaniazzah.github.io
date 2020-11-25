18ede3de2ec9853c4ff4e1c6e78c151c20f0c756
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-11-25 09:38:05 +0700

    Create linktiga.js

---
+++ js/linktiga.js
@@ -0,0 +1,19 @@
+function setAttributes(el,attrs){for(var key in attrs){el.setAttribute(key,attrs[key]);}}
+
+var aads1 = document.createElement('iframe');
+setAttributes(aads1, {
+  "data-aa":"1515377",
+  "scrolling":"no",
+  "src":"//ad.a-ads.com/1515377?size=320x50",
+  "style":"width:320px;height:50px;"
+});
+document.getElementById('l3ads1').appendChild(aads1);
+
+var aads2 = document.createElement('iframe');
+setAttributes(aads2, {
+  "data-aa":"1515379",
+  "scrolling":"no",
+  "src":"//ad.a-ads.com/1515379?size=320x50",
+  "style":"width:320px;height:50px;"
+});
+document.getElementById('l3ads2').appendChild(aads2);
