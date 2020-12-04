16771e13a201925c8237ad6fbf0ce6b7e24cfa60
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-12-04 13:44:06 +0700

    Update post_1.js

--- l3/js/post_1.js
+++ l3/js/post_1.js
@@ -1,1 +1,3 @@
-
+$(".l3v_yt").each(function() {
+        $(this).replaceWith('<amp-youtube data-videoid="' + $(this).data("id") + '" layout="responsive" width="480" height="270" autoplay></amp-youtube>')
+    })
