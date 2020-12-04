93d30646e23bf2aa5c73b7b96931bc2f5c73a152
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-12-04 15:53:57 +0700

    Update post.js

--- l3/post.js
+++ l3/post.js
@@ -1,3 +1,4 @@
 l3file("https://cdn.ampproject.org/v0.js", "js")
 l3file("https://cdn.ampproject.org/v0/amp-youtube-0.1.js", "js")
+l3file("https://cdn.ampproject.org/v0/amp-social-share-0.1.js", "js")
 l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/js/post_1.js", "js")
