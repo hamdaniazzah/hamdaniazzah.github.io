243b9f17481fc1a8d7b455e8b1316e565b6928d2
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-12-14 15:34:06 +0700

    Update post_lirik.js

--- l3/post_lirik.js
+++ l3/post_lirik.js
@@ -1,3 +1,3 @@
-l3file("https://github.com/hamdaniazzah/hamdaniazzah.github.io/blob/master/l3/css/post_lirik_1.css", "css")
+l3file("https://hamdaniazzah.github.io/l3/css/post_lirik_1.css", "css")
 l3file("https://hamdaniazzah.github.io/l3/js/post_lirik_1.js", "js")
 l3file("https://cdn.ampproject.org/v0/amp-bind-0.1.js", "js")
