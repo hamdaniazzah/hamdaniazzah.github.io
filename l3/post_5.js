6527d81a55365666c92fd64226a6f4b466c043da
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-12-16 07:08:41 +0700

    Update and rename post_4.js to post_5.js

--- l3/post_5.js
+++ l3/post_5.js
@@ -2,5 +2,6 @@ l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/
 l3file("https://cdn.ampproject.org/v0.js", "js")
 l3file("https://cdn.ampproject.org/v0/amp-youtube-0.1.js", "js")
 l3file("https://cdn.ampproject.org/v0/amp-iframe-0.1.js", "js")
+l3file("https://cdn.ampproject.org/v0/amp-facebook-like-0.1.js", "js")
 l3file("https://cdn.ampproject.org/v0/amp-social-share-0.1.js", "js")
 l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/js/post_1.js", "js")
