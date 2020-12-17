3176db581399a2ab1c8a62327cfc41c2a50a776e
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-12-17 15:52:57 +0700

    Update and rename gen_2.js to gen_3.js

--- l3/gen_3.js
+++ l3/gen_3.js
@@ -1,3 +1,3 @@
 l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/css/gen_3.css", "css")
-l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/js/gen_1.js", "js")
+l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/js/gen_2.js", "js")
 l3file("https://www.googletagmanager.com/gtag/js?id=G-GC3PR9DQEJ", "js")
