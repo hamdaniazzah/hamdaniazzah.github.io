ac854675b17c7e51dc3d062356097cfc8d61d835
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2020-12-19 07:05:27 +0700

    Update and rename gen_3.js to gen_4.js

--- l3/gen_4.js
+++ l3/gen_4.js
@@ -1,3 +1,4 @@
+l3file("https://www.googletagmanager.com/gtag/js?id=G-GC3PR9DQEJ", "js")
 l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/css/gen_3.css", "css")
 l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/js/gen_2.js", "js")
-l3file("https://www.googletagmanager.com/gtag/js?id=G-GC3PR9DQEJ", "js")
+l3file("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js", "js")
