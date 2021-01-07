4f860849a29057983c10d90705314d5ea83c3aef
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2021-01-07 13:05:49 +0700

    Update and rename diisi_1.js to diisi_2.js

--- l3/diisi_2.js
+++ l3/diisi_2.js
@@ -1,3 +1,2 @@
-$("<div>", {id:"bp_toc"}).insertAfter(".l3_halaman");
 l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/css/diisi_1.css", "css")
 l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/js/diisi_1.js", "js")
