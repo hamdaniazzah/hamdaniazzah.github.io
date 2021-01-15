a88e76af39d4dd634aba9e7339d154d6ecf182b4
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2021-01-16 00:15:12 +0700

    Update and rename post_6.js to post_7.js

--- l3/js/post_7.js
+++ l3/js/post_7.js
@@ -18,7 +18,19 @@ $(".faqnya").each(function() {
 
 $("#l3author").each(function(){$(this).replaceWith('<div class="l3author teks_stroke">Penulis:</div><div class="l3_author"><amp-img alt="Penulis: ' + $(this).data("name") + '" src="' + $(this).data("img") + '" width="50" height="50" layout="responsive" onContextMenu="return false"></amp-img><div><a href="' + $(this).data("url") + '" rel="nofollow noopener" target="_blank">' + $(this).data("name") + '</a><div>' + $(this).data("job") + '</div></div></div>')})
 
-$("<div class='as_1'></div>").insertAfter("#as_1");
-$("<div class='as_2'></div>").insertAfter("#as_2");
-$(".as_1").replaceWith('<div class="l3_as" align="center"><amp-ad width="100vw" height="320" type="adsense" data-ad-client="ca-pub-4881038805263910" data-ad-slot="4117181124" data-auto-format="rspv" data-full-width=""><div overflow=""></div></amp-ad></div>');
-$(".as_2").replaceWith('<div class="l3_as" align="center"><amp-ad width="100vw" height="320" type="adsense" data-ad-client="ca-pub-4881038805263910" data-ad-slot="8314945422" data-auto-format="rspv" data-full-width=""><div overflow=""></div></amp-ad></div>');
+var as1 = '<div class="l3_as" align="center"><amp-ad width="100vw" height="320" type="adsense" data-ad-client="ca-pub-4881038805263910" data-ad-slot="4117181124" data-auto-format="rspv" data-full-width=""><div overflow=""></div></amp-ad></div>';
+var as2 = '<div class="l3_as" align="center"><amp-ad width="100vw" height="320" type="adsense" data-ad-client="ca-pub-4881038805263910" data-ad-slot="8314945422" data-auto-format="rspv" data-full-width=""><div overflow=""></div></amp-ad></div>';
+
+$("<div class='as_1'></div>").insertAfter("#as_1a");
+$("<div class='as_2'></div>").insertAfter("#as_2a");
+$("<div class='as_1'></div>").insertBefore("#as_1b");
+$("<div class='as_2'></div>").insertBefore("#as_2b");
+$("<div id='as_1'></div>").insertAfter(".as_1a");
+$("<div id='as_2'></div>").insertAfter(".as_2a");
+$("<div id='as_1'></div>").insertBefore(".as_1b");
+$("<div id='as_2'></div>").insertBefore(".as_2b");
+
+$(".as1").each(function(){$(this).replaceWith(as1)});
+$(".as2").each(function(){$(this).replaceWith(as2)});
+$("#as1").each(function(){$(this).replaceWith(as1)});
+$("#as2").each(function(){$(this).replaceWith(as2)});
