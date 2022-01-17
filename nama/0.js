03243c7d3244c92fba6a924686867a3254e9aea0
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-01-17 15:34:39 +0700

    Create 0.js

---
+++ nama/0.js
@@ -0,0 +1,12 @@
+loadyn(cdn+"css/gen3.min.css","css");
+
+if(!(ls.indexOf("exp=" || "ext2" || "ext3")>-1)){
+loadyn(cdns+gh0+"css/home5.min.css","css");
+}
+
+if(ls.indexOf("exp=")>-1){
+loadyn(cdns+"css/exp1.min.css","css");
+loadyn(cdns+"js/exp2.min.js","js");
+}
+
+loadyn(cdns+"js/last0.min.js","js");
