1f37ead2bcd6e25467367fd214f23f124901a3b2
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-01-17 16:03:15 +0700

    Update 0.js

--- nama/0.js
+++ nama/0.js
@@ -9,4 +9,4 @@ loadyn(cdns+"css/exp1.min.css","css");
 loadyn(cdns+"js/exp2.min.js","js");
 }
 
-loadyn(cdns+"js/last0.min.js","js");
+setTimeout(function(){const e=doc.getElementById("preloader").style,t=(t,o)=>e.setProperty(t,o),o="transition",i="4.5s",s="visible";t("-webkit-"+o,i),t("-moz-"+o,i),t("-ms-"+o,i),t("-o-"+o,i),t(o,i),t("opacity","0"),t("overflow",s),t("visibility","hidden"),doc.getElementsByTagName("body")[0].style.overflow=s},1500);
