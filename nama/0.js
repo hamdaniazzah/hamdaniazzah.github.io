e1e249ac07f2f4db49a97a77287ad1139619a9ff
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-01-24 21:33:56 +0700

    Update 0.js

--- nama/0.js
+++ nama/0.js
@@ -6,7 +6,7 @@ loadyn(cdn+"js/gen2"+ft2,"js");
 
 if(!(ls.indexOf("exp=" || "ext2" || "ext3")>-1)){
 loadyn(cdn+"css/home6"+ft1,"css");
-loadyn(cdn+"js/home9"+ft2,"js");
+loadyn(cdn+"js/home10"+ft2,"js");
 }
 
 if(ls.indexOf("exp=")>-1){
