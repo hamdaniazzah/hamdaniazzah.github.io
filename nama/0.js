cacb877284d654e5472c3b27df1bf41a2ba9f2d1
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-02-14 06:17:12 +0700

    Update 0.js

--- nama/0.js
+++ nama/0.js
@@ -1,7 +1,7 @@
 var ft1 = ".min.css",
     ft2 = ".min.js";
 loadyn(cdn+"css/gen8"+ft1,"css");
-loadyn(cdn+"js/gen10"+ft2,"js");
+loadyn(cdn+"js/gen11"+ft2,"js");
 if(!(ls.indexOf("exp=" || "ext2" || "ext3")>-1)){
 loadyn(cdn+"css/home7"+ft1,"css");
 loadyn(cdn+"js/home37"+ft2,"js");
