1975343b82c73d17117b0d17319836e8f3a1feae
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-03-19 19:16:50 +0700

    Update 0.js

--- nama/0.js
+++ nama/0.js
@@ -1,13 +1,13 @@
 var ft1 = ".min.css",
     ft2 = ".min.js";
 loadyn(cdn+"css/gen8"+ft1,"css");
-loadyn(cdn+"js/gen11"+ft2,"js");
+loadyn(cdn+"js/gen12"+ft2,"js");
 if(!(ls.indexOf("exp=" || "ext2" || "ext3")>-1)){
 loadyn(cdn+"css/home7"+ft1,"css");
 loadyn(cdn+"js/home45"+ft2,"js");
 }
 if(ls.indexOf("exp=")>-1){
 loadyn(cdn+"css/exp1"+ft1,"css");
-loadyn(cdn+"js/exp28"+ft2,"js");
+loadyn(cdn+"js/exp29"+ft2,"js");
 }
 loadyn(cdn+"js/last2"+ft2,"js");
