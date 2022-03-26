5561393282d3c871238d4cd52683b452429e7959
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-03-26 07:57:20 +0700

    Update 0.js

--- nama/0.js
+++ nama/0.js
@@ -6,21 +6,21 @@ loadyn(cdn+"css/gen13"+ft1,"css");
 loadyn(cdn+"js/gen12"+ft2,"js");
 
 if(
-!(lh.match(/\bpay$/)) &&
-!(ls.match(/\bexp=/))
+!(ls.match(/\bexp/)) &&
+!(lh.match(/\bpay$/))
 ){
 loadyn(cdn+"css/home9"+ft1,"css");
 loadyn(cdn+"js/home53"+ft2,"js");
 }
 
-if(lh.match(/\bpay$/)){
-loadyn(cdn+"css/pay0"+ft1,"css");
-loadyn(cdn+"js/pay0"+ft2,"js");
-}
-
 if(ls.match(/\bexp=/)){
 loadyn(cdn+"css/exp1"+ft1,"css");
 loadyn(cdn+"js/exp34"+ft2,"js");
+}
+
+if(lh.match(/\bpay$/)){
+loadyn(cdn+"css/pay0"+ft1,"css");
+loadyn(cdn+"js/pay0"+ft2,"js");
 }
 
 loadyn(cdn+"js/last2"+ft2,"js");
