3c9febad80978bcb44fa44cee1882cfedd0948d3
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-03-27 13:08:56 +0700

    Update 0.js

--- nama/0.js
+++ nama/0.js
@@ -2,7 +2,7 @@ var ft1 = ".min.css",
     ft2 = ".min.js",
     lh = location.hash;
 
-loadyn(cdn+"css/gen17"+ft1,"css");
+loadyn(cdn+"css/gen18"+ft1,"css");
 loadyn(cdn+"js/gen12"+ft2,"js");
 
 if(
@@ -10,7 +10,7 @@ if(
 !(lh.match(/\bpay$/))
 ){
 loadyn(cdn+"css/home12"+ft1,"css");
-loadyn(cdn+"js/home56"+ft2,"js");
+loadyn(cdn+"js/home57"+ft2,"js");
 }
 
 if(ls.match(/\bexp=/)){
