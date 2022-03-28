c65b53b26b962d7daf7cfe7e806cca931e023201
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-03-28 15:23:05 +0700

    Update 0.js

--- nama/0.js
+++ nama/0.js
@@ -20,7 +20,7 @@ loadyn(cdn+"js/exp35"+ft2,"js");
 
 if(lh.match(/\bpay$/)){
 loadyn(cdn+"css/pay9"+ft1,"css");
-loadyn(cdn+"js/pay14"+ft2,"js");
+loadyn(cdn+"js/pay15"+ft2,"js");
 }
 
 loadyn(cdn+"js/last2"+ft2,"js");
