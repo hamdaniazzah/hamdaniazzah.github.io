31f6737f78115ff50d1a6ddf323ec6e835400358
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-05-29 13:49:40 +0700

    Update 0.js

--- kocek/0.js
+++ kocek/0.js
@@ -1,3 +1,4 @@
+loadyn("cdn.statically.io/gh/elad2412/the-new-css-reset/main/css/reset"+ft1,"css");
 loadyn(cdn+"css/all0"+ft1,"css");
 loadyn(cdn+"js/all0"+ft2,"js");
 loadyn(cdn+"js/home0"+ft1,"css");
