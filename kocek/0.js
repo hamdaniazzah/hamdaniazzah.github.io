8731e8ca7a6ca6c555345df6e7778c31a7b3f9a5
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-05-29 15:13:39 +0700

    Delete 0.js

--- kocek/0.js
+++
@@ -1,6 +0,0 @@
-loadyn("cdn.statically.io/gh/elad2412/the-new-css-reset/main/css/reset"+ft1,"css");
-
-loadyn(cdn+"css/all0"+ft1,"css");
-loadyn(cdn+"js/all1"+ft2,"js");
-
-loadyn(cdn+"css/home0"+ft1,"css");
