d1d37866581ecee82be5ba85e27d289510ad5900
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-05-31 10:14:47 +0700

    Update id.js

--- kk/id.js
+++ kk/id.js
@@ -1,1 +1,21 @@
+if(
+!(ls.match(/\bp=/)) &&
+!(ls.match(/\bq=/)) &&
+!(ls.match(/\bforms=/))
+){
 
+}
+
+//
+
+if(ls.match(/\bp=/)){
+
+}
+
+if(ls.match(/\bq=/)){
+
+}
+
+if(ls.match(/\bforms=/)){
+
+}
