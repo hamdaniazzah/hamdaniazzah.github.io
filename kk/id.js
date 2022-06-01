328c101c1dea7218375f3a2b9aeb6ea41e38c38e
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-01 12:42:50 +0700

    Update id.js

--- kk/id.js
+++ kk/id.js
@@ -3,19 +3,27 @@ if(
 !(ls.match(/\bq=/)) &&
 !(ls.match(/\bforms=/))
 ){
-
+//
 }
 
-//
-
 if(ls.match(/\bp=/)){
+
+if(ls.match(/\bp=about$/)){
+loadyn(k1+'p/about0.min','b');
+}
 
 }
 
 if(ls.match(/\bq=/)){
-
 }
 
 if(ls.match(/\bforms=/)){
+}
 
+if (typeof idSetting === 'undefined') {
+loadyn(k1+'b/none0.min','b');
+}
+
+if (typeof idSetting !== 'undefined') {
+loadyn(k1+'b/idlast0.min','b');
 }
