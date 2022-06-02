51f9d8ae9c5be74f0f1c681682f3a5ad0e8e37af
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-02 08:10:41 +0700

    Update 0.js

--- kk/0.js
+++ kk/0.js
@@ -2,3 +2,4 @@ loadyn(cdn+'elad2412/the-new-css-reset/main/css/reset.min','a');
 loadyn(kk1+'a/all0.min','a');
 loadyn(kk1+'b/all3.min','b');
 loadyn(gio+kdataid,'b');
+loadyn(kk1+'b/klast0.min','b');
