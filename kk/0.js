150cdcbafc92562fc24330501c89ba6f2b9e08bd
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-06-09 10:41:03 +0700

    Update 0.js

--- kk/0.js
+++ kk/0.js
@@ -1,4 +1,4 @@
 loadyn(kk1+'a/all7.min','a');
-loadyn(kk1+'b/all3.min','b');
+loadyn(kk1+'b/all4.min','b');
 loadyn(gio+kdataid,'b');
-loadyn(kk1+'b/klast0.min','b');
+loadyn(kk1+'b/klast1.min','b');
